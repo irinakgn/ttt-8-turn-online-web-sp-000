5c76439741740eed779a0e9941c0be8f68acb42e
mirthful-flag-5763 <irinagamb@gmail.com>
2019-03-22 23:46:04 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -22,22 +22,11 @@ def move(board, index, value)
   board[index] = value
 end
 
-def turn(board)
-
+def turn()
   loop  do
     puts "Please enter 1-9:"
     input = gets.chomp
     index = input_to_index(input)
     break if index > -1
   end
-
-
 end
-
-# get input
-# convert input to index
-# if index is valid
-#   make the move for input
-# else
-#   ask for input again until you get a valid input
-# end
