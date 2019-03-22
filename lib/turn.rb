09fbfa172b8afc2075104a839b078dc9df051bc8
mirthful-flag-5763 <irinagamb@gmail.com>
2019-03-22 22:45:58 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,17 @@
+
+
+
+def display_board(board)
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+   Integer(user_input || '') -1
+    rescue ArgumentError
+    nil
+end
